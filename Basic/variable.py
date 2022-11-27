e21977dea347ece193f6d7632ef7e1abccf631b1
Rakib Hasan <rakibhasan1030@gmail.com>
2022-11-27 22:43:04 +0600

    Basic/variable: create

---
+++ Basic/variable.py
@@ -0,0 +1,8 @@
+name = "Rakib Hasan"
+age = 26
+balance = 100000000.00
+is_male = True
+
+#print("Name : " + name + "\nAge : " + age + "\nBalance : " + balance + "\n Is male? : " + is_male)
+print("Name : " + str(name) + "\nAge : " + str(age) +
+      "\nBalance : " + str(balance) + "\nIs male? : " + str(is_male))
