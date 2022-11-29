58defbbb6b65f4cd8d5dd05d974c531b2f41bc90
Rakib Hasan <rakibhasan1030@gmail.com>
2022-11-29 23:33:38 +0600

    Advance/05_try_except_finally:

--- Advance/05_try_except_finally.py
+++ Advance/05_try_except_finally.py
@@ -8,4 +8,7 @@ finally:
     # finally will execute at any cost though the program exit in exeption, it'll execute
     # it used for clean-up. suppose program use some service and then you've to close the service before exit the program
     # here finally works well
+    # again if program exit in exept bloc, then only finally bloc will be execte, no code will execute after finally bloc
     print("We're done!")
+
+print("This messege will not print because exept bloc program perform exit!")
