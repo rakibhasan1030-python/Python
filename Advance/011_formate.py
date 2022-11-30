29f78da646c541ff754c8a5b1af0449d5fb1f4ee
Rakib Hasan <rakibhasan1030@gmail.com>
2022-11-30 22:10:52 +0600

    Advance/011_formate: create

---
+++ Advance/011_formate.py
@@ -0,0 +1,8 @@
+name1 = "Rakib"
+name2 = "Khalid"
+name3 = "Sohel"
+name4 = "Khadimul"
+
+print("Names =  {} : {} : {} : {}".format(name1, name2, name3, name4))
+print("Names =  {3} : {2} : {1} : {0}".format(name1, name2, name3, name4))
+print("Names =  {2} : {1} : {0} : {0}".format(name1, name2, name3, name4))
