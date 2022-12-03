146f44fe2776d30c1788c75d7f2ea2fea5fb2a76
Rakib Hasan <rakibhasan1030@gmail.com>
2022-12-03 14:15:53 +0600

    Advance/08_list_comprehension:

--- Advance/08_list_comprehension.py
+++ Advance/08_list_comprehension.py
@@ -1,3 +1,5 @@
+# syntax = [expression for item in iterable condition(if have)]
+
 data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 evenData = []
 oddData = []
@@ -13,3 +15,4 @@ print(f"Even numbers : {evenData}\nOdd numbers : {evenData}")
 print("\n\nComprehensive approach:")  # one liner or shortcut
 evenData = [i for i in data if i % 2 == 0]
 evenData = [i for i in data if i % 2 != 0]
+print(f"Even numbers : {evenData}\nOdd numbers : {evenData}")
